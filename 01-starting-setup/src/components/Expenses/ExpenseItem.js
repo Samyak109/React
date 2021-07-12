0e8bb6b46f8ed1aa2b139db7c331373e675fc849
Samyak Jain <samyak915jain@gmail.com>
2021-07-12 16:45:47 +0530

    added add new expense with form logic and used state to store data

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler(e) {
+    setTitle("Updated"); // assign new value and re-render the component in which it is called and initialized
+    // state update is not synchronous, it is scheduled
+    console.log("Clicked");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}> Change Title </button>
     </Card>
   );
 }
